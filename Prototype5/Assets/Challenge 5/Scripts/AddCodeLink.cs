26099e309111505f3464f2eccce9b47840b5f875
oisinegan <oisinegan00@gmail.com>
2021-10-28 16:24:48 +0100

    Added Button to prototype 5 repo

---
+++ Prototype5/Assets/Challenge 5/Scripts/AddCodeLink.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddCodeLink : MonoBehaviour
+{
+    public void addCodeRepo()
+    {
+        Application.OpenURL("https://github.com/oisinegan/Unit5-CreateWithCode");
+    }
+}
